f34097cc36c88d3699b06627d811e0abc3edda63
Anthony Guerra <anthony.gue@lvjusd.k12.ca.us>
2018-12-04 12:40:45 -0800

    Yote

---
+++ yuh.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class yuh here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class yuh
+{
+   public static void yuh()
+   {
+       System.out.println("Yeet");
+    }
+}
